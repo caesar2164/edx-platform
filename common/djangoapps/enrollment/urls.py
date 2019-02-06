501858dc27ab287315438157251a2dacce1e8bc7
stv <stv@stanford.edu>
2019-02-06 00:54:32 -0800

    Add line: common/djangoapps/enrollment/urls.py

--- common/djangoapps/enrollment/urls.py
+++ common/djangoapps/enrollment/urls.py
@@ -3,12 +3,8 @@ URLs for the Enrollment API
 
 """
 from django.conf import settings
-<<<<<<< HEAD
 from django.conf.urls import include
-from django.conf.urls import patterns, url
-=======
 from django.conf.urls import url
->>>>>>> 896e66f8fcc1d2828d9c8299da0187ba96e8156e
 
 from .views import EnrollmentCourseDetailView, EnrollmentListView, EnrollmentView, UnenrollmentView
 
@@ -20,19 +16,8 @@ urlpatterns = [
     url(r'^enrollment/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentView.as_view(), name='courseenrollment'),
     url(r'^enrollment$', EnrollmentListView.as_view(), name='courseenrollments'),
-<<<<<<< HEAD
-    url(
-        r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
-        EnrollmentCourseDetailView.as_view(),
-        name='courseenrollmentdetails'
-    ),
-)
-urlpatterns += (
-    url(r'', include('openedx.stanford.common.djangoapps.enrollment.urls')),
-)
-=======
     url(r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentCourseDetailView.as_view(), name='courseenrollmentdetails'),
     url(r'^unenroll/$', UnenrollmentView.as_view(), name='unenrollment'),
 ]
->>>>>>> 896e66f8fcc1d2828d9c8299da0187ba96e8156e
+urlpatterns += [url(r'', include('openedx.stanford.common.djangoapps.enrollment.urls')),]
