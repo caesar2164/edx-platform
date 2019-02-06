29b324bcdff0a7de43ff438851202dae94f93d0c
stv <stv@stanford.edu>
2019-02-06 00:54:48 -0800

    Take theirs: lms/djangoapps/grades/signals/handlers.py

--- lms/djangoapps/grades/signals/handlers.py
+++ lms/djangoapps/grades/signals/handlers.py
@@ -10,13 +10,7 @@ from django.dispatch import receiver
 from submissions.models import score_reset, score_set
 from xblock.scorable import ScorableXBlockMixin, Score
 
-<<<<<<< HEAD
-from courseware.model_data import get_score, set_score
-from eventtracking import tracker
-from lms.djangoapps.instructor_task.tasks_helper.module_state import GRADES_OVERRIDE_EVENT_TYPE
-=======
 from openedx.core.djangoapps.course_groups.signals.signals import COHORT_MEMBERSHIP_UPDATED
->>>>>>> 896e66f8fcc1d2828d9c8299da0187ba96e8156e
 from openedx.core.lib.grade_utils import is_score_higher_or_equal
 from student.models import user_by_anonymous_id
 from student.signals import ENROLLMENT_TRACK_UPDATED
@@ -252,49 +246,10 @@ def recalculate_course_and_subsection_grades(sender, user, course_key, countdown
     Updates a saved course grade, forcing the subsection grades
     from which it is calculated to update along the way.
     """
-<<<<<<< HEAD
-    root_type = get_event_transaction_type()
-
-    if not root_type:
-        root_id = get_event_transaction_id()
-        if not root_id:
-            root_id = create_new_event_transaction_id()
-        set_event_transaction_type(PROBLEM_SUBMITTED_EVENT_TYPE)
-        tracker.emit(
-            unicode(PROBLEM_SUBMITTED_EVENT_TYPE),
-            {
-                'user_id': unicode(kwargs['user_id']),
-                'course_id': unicode(kwargs['course_id']),
-                'problem_id': unicode(kwargs['usage_id']),
-                'event_transaction_id': unicode(root_id),
-                'event_transaction_type': unicode(PROBLEM_SUBMITTED_EVENT_TYPE),
-                'weighted_earned': kwargs.get('weighted_earned'),
-                'weighted_possible': kwargs.get('weighted_possible'),
-            }
-        )
-
-    if root_type in [GRADES_RESCORE_EVENT_TYPE, GRADES_OVERRIDE_EVENT_TYPE]:
-        current_user = get_current_user()
-        instructor_id = getattr(current_user, 'id', None)
-        tracker.emit(
-            unicode(GRADES_RESCORE_EVENT_TYPE),
-            {
-                'course_id': unicode(kwargs['course_id']),
-                'user_id': unicode(kwargs['user_id']),
-                'problem_id': unicode(kwargs['usage_id']),
-                'new_weighted_earned': kwargs.get('weighted_earned'),
-                'new_weighted_possible': kwargs.get('weighted_possible'),
-                'only_if_higher': kwargs.get('only_if_higher'),
-                'instructor_id': unicode(instructor_id),
-                'event_transaction_id': unicode(get_event_transaction_id()),
-                'event_transaction_type': unicode(root_type),
-            }
-=======
     recalculate_course_and_subsection_grades_for_user.apply_async(
         countdown=countdown,
         kwargs=dict(
             user_id=user.id,
             course_key=six.text_type(course_key)
->>>>>>> 896e66f8fcc1d2828d9c8299da0187ba96e8156e
         )
     )
