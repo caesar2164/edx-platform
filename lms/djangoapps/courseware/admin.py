b9f294408aa3f7e97eb47b206c83c8886dfe2a4d
stv <stv@stanford.edu>
2019-02-06 00:54:42 -0800

    Add line: lms/djangoapps/courseware/admin.py

--- lms/djangoapps/courseware/admin.py
+++ lms/djangoapps/courseware/admin.py
@@ -2,24 +2,12 @@ from django.contrib import admin
 
 from config_models.admin import ConfigurationModelAdmin, KeyedConfigurationModelAdmin
 
-<<<<<<< HEAD
-from courseware.models import CoursePreference
-from courseware.models import OfflineComputedGrade, OfflineComputedGradeLog, StudentModule
-
-admin.site.register(StudentModule)
-
-admin.site.register(OfflineComputedGrade)
-
-admin.site.register(OfflineComputedGradeLog)
-
-admin.site.register(CoursePreference)
-=======
 from courseware import models
 
+admin.site.register(models.CoursePreference)
 admin.site.register(models.DynamicUpgradeDeadlineConfiguration, ConfigurationModelAdmin)
 admin.site.register(models.OfflineComputedGrade)
 admin.site.register(models.OfflineComputedGradeLog)
 admin.site.register(models.CourseDynamicUpgradeDeadlineConfiguration, KeyedConfigurationModelAdmin)
 admin.site.register(models.OrgDynamicUpgradeDeadlineConfiguration, KeyedConfigurationModelAdmin)
 admin.site.register(models.StudentModule)
->>>>>>> 896e66f8fcc1d2828d9c8299da0187ba96e8156e
